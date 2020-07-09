2601028393248a37a17bd062e84a4059435e7b34
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-10 00:22:23 +0100

    writing rnn for H2 data

--- Project_1_RBM_and_Tomography/rnn_helper.py
+++ Project_1_RBM_and_Tomography/rnn_helper.py
@@ -1,26 +1,37 @@
 import tensorflow as tf
 from tensorflow.keras import layers
+import torch
 import numpy as np
 import os
 
+import H2_energy_calculator
+
+BATCH_SIZE=10
 
 class RNN:
 
     def __init__(self, n_vis: int, n_hdn: int):
         self.n_vis = n_vis
         self.n_hdn = n_hdn
+        self.rnn = self.model()
 
     def model(self):
         model = tf.keras.Sequential()
-        model.add(layers.InputLayer(input_shape=(None, self.n_vis)))
-        model.add(layers.Bidirectional(layers.SimpleRNN(self.n_hdn)))
+        model.add(layers.InputLayer(input_shape=(BATCH_SIZE, self.n_vis)))
+        model.add(layers.SimpleRNN(self.n_hdn, return_sequences=True))
         model.add(layers.Dense(self.n_vis))
         return model
+
+    @staticmethod
+    def linear(inp, weight, bias):
+        output = tf.matmul(inp, tf.transpose(weight))
+        output += bias
+        return output
 
 
 class RNNHelper:
 
-    def __init__(self, epochs: int = 500, n_samples: int = 1000, lr: float = 0.03, n_hdn: int = 6, verbose: bool = False,
+    def __init__(self, epochs: int = 15, n_samples: int = 1000, lr: float = 0.03, n_hdn: int = 6, verbose: bool = False,
                  loc: str = './'):
         self.epochs = epochs
         self.n_samples = n_samples
@@ -28,47 +39,66 @@ class RNNHelper:
         self.n_hdn = n_hdn
         self.v = verbose
         self.loc = loc
+        self.loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        self.optimiser = tf.keras.optimizers.Adam(self.lr)
 
     @staticmethod
     def return_coeffs_samples():
         loc = './H2_data/'
         coeffs = np.loadtxt(os.path.join(loc, 'H2_coefficients.txt'))
         samples = []
+        true_energies = []
         for c in coeffs:
             r = c[0]
             np_data = np.loadtxt(os.path.join(loc, 'R_{}_samples.txt'.format(str(r))))
+            true_energy = H2_energy_calculator.energy_from_freq(torch.from_numpy(np_data), c)
+            true_energies.append(true_energy)
             train_len, test_len = int(0.7 * len(np_data)), int(0.3 * len(np_data))
-            sample = tf.data.Dataset.from_tensor_slices([np_data])
+            sample = tf.data.Dataset.from_tensor_slices(np_data)
             train, test = sample.take(train_len), sample.take(test_len)
-            train, test = train.padded_batch(10), test.padded_batch(10)
+            train, test = train.batch(BATCH_SIZE), test.batch(BATCH_SIZE)
             samples.append((train, test))
-        return coeffs, samples
+        return coeffs, samples, true_energies
 
     def iterate_over_r(self):
-        coeffs, samples = self.return_coeffs_samples()
-        n_vis = samples[0][0].element_spec.shape[-1]
+        coeffs, samples, true_energies = self.return_coeffs_samples()
+        self.n_vis = samples[0][0].element_spec.shape[-1]
 
-        for coeff, (train, test) in zip(coeffs, samples):
-            rnn = RNN(n_vis, self.n_hdn)
-            model = rnn.model()
-            model = self.train_model(model, train, test)
-            # model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
-            #               optimizer=tf.keras.optimizers.Adam(self.lr), )
-            # history = model.fit(train, epochs=self.epochs,
-            #                     validation_data=test, validation_steps=30)
+        for coeff, (train, test), true_energy in zip(coeffs, samples, true_energies):
+            r = coeff[0]
+            print('\n\nR = {}'.format(r))
+            rnn = RNN(self.n_vis, self.n_hdn)
+            model = rnn.rnn
+            for e in range(self.epochs):
+                print('Start of epoch {}'.format(e))
+                for step, x_batch in enumerate(train):
+                    loss = self.train_step(model, x_batch)
+                    if step % 100 == 0:
+                        energies = []
+                        acc = []
+                        for init in test:
+                            test = tf.reshape(init, (1, BATCH_SIZE, self.n_vis))
+                            predictions = model.predict_on_batch(test)
+                            clip_pred = tf.clip_by_value(predictions, 0, 1)
+                            torch_pred = torch.from_numpy(clip_pred.numpy())
+                            energy = H2_energy_calculator.energy_from_freq(torch_pred, coeff)
+                            energies.append(energy)
+                            accuracy = 1 - np.abs(np.abs(energy - true_energy) / true_energy)
+                            acc.append(accuracy)
+                        print('Step: {}, loss: {}\nEnergy: {}\nAccuracy: {}'.format(step, loss,
+                                                                                    tf.reduce_mean(energies),
+                                                                                    tf.reduce_mean(acc)))
+            model.save('/home/andrew/Documents/PhD/CDLQuantum/saved_models/rnn_r_{}'.format(r))
 
-    def train_model(self, model: tf.keras.Model, train: tf.data.Dataset, test: tf.data.Dataset):
-        loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-        optimiser = tf.keras.optimizers.Adam(self.lr)
-        for e in range(self.epochs):
-            for step, x_batch in enumerate(train):
-                with tf.GradientTape() as tape:
-                    sample = model(x_batch)
-                    batch_expectation = tf.reduce_mean(x_batch, 1)
-                    loss = loss_fn(batch_expectation, sample)
-                grads = tape.gradient(loss, model.trainable_weights)
-                optimiser.apply_gradients(zip(grads, model.trainable_weights))
-        return model
+    # @tf.function
+    def train_step(self, model, batch):
+        with tf.GradientTape() as tape:
+            batch = tf.reshape(batch, (1, BATCH_SIZE, self.n_vis))
+            sample = model(batch)
+            loss = tf.reduce_mean(self.loss_fn(batch,  sample))
+        grads = tape.gradient(loss, model.trainable_weights)
+        self.optimiser.apply_gradients(zip(grads, model.trainable_weights))
+        return loss
 
 
 if __name__ == '__main__':
